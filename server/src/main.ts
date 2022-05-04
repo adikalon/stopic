d7bf5f9d1ab5b55e9854a92bc5aa3839c1ce64dc
adikalon <webadev@yandex.ru>
2022-05-04 20:05:06 +0300

    Настроен глобальный ValidationPipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DefaultExceptionFilter } from './common/filters/default-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      disableErrorMessages: false,
+      validationError: {
+        value: true,
+      },
+      transform: true,
+    }),
+  );
   app.setGlobalPrefix('api');
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new DefaultExceptionFilter(httpAdapter));
