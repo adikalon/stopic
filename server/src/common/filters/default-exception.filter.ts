732c8784851f2ce1ce8154af7f9d4affd5c8230a
adikalon <webadev@yandex.ru>
2022-05-07 01:03:19 +0300

    Текст ошибки в фильтре ошибок

--- server/src/common/filters/default-exception.filter.ts
+++ server/src/common/filters/default-exception.filter.ts
@@ -46,6 +46,8 @@ export class DefaultExceptionFilter extends BaseExceptionFilter {
         }
 
         res = JSON.stringify(res, null, 2);
+      } else if (typeof res === 'string') {
+        resp = res;
       }
 
       error = res;
