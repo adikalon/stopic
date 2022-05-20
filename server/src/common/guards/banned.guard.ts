54906bf7edc78e4d82f15b92af89b5d881d78300
adikalon <webadev@yandex.ru>
2022-05-21 00:15:06 +0300

    Новый охранник для блокировки

--- server/src/common/guards/banned.guard.ts
+++ server/src/common/guards/banned.guard.ts
@@ -2,37 +2,21 @@ import {
   Injectable,
   CanActivate,
   ExecutionContext,
-  HttpException,
-  HttpStatus,
+  ForbiddenException,
 } from '@nestjs/common';
 import { BannedService } from '../../modules/banned/banned.service';
-import { VisitorService } from '../../modules/visitor/visitor.service';
+import { VisitorRequest } from '../../modules/visitor/visitor-request.interface';
 
 @Injectable()
 export class BannedGuard implements CanActivate {
-  constructor(
-    private readonly visitorService: VisitorService,
-    private readonly bannedService: BannedService,
-  ) {}
+  constructor(private readonly bannedService: BannedService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const req = context.switchToHttp().getRequest();
-    const ip = req.clientIp;
-    const userAgent = req.header('user-agent');
+    const req: VisitorRequest = context.switchToHttp().getRequest();
+    const banned = await this.bannedService.getVisitorBanned(req.visitor);
 
-    if (ip && userAgent) {
-      const visitor = await this.visitorService.getVisitor(ip, userAgent);
-
-      if (visitor) {
-        const banned = await this.bannedService.getVisitorBanned(visitor);
-
-        if (banned) {
-          throw new HttpException(
-            `Access blocked to ${banned.bannedTo}`,
-            HttpStatus.FORBIDDEN,
-          );
-        }
-      }
+    if (banned) {
+      throw new ForbiddenException(`Access blocked to ${banned.bannedTo}`);
     }
 
     return true;
