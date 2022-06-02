d621168a57a3c8b06b58babe2277c3f105e403fa
adikalon <webadev@yandex.ru>
2022-06-02 23:56:56 +0300

    Очистка temp на промисах

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,49 +1,22 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import * as path from 'path';
-import * as fs from 'fs';
+import * as fsPromises from 'fs/promises';
 
 @Injectable()
 export class AppService {
-  private readonly logger = new Logger(AppService.name);
-
   @Cron('0 0 0 * * *')
   async clearTemp() {
     const pathFolder = path.join(__dirname, '/../temp/');
 
-    fs.access(pathFolder, (err) => {
-      if (err) {
-        return;
+    (await fsPromises.readdir(pathFolder)).forEach(async (file) => {
+      const pathFile = path.join(pathFolder, file);
+      const stats = await fsPromises.stat(pathFile);
+      const diff = Math.abs(new Date().getTime() - stats.mtime.getTime());
+
+      if (stats.isFile() && diff > 3600000) {
+        await fsPromises.unlink(pathFile);
       }
-
-      fs.readdir(pathFolder, (err, files) => {
-        if (err) {
-          this.logger.warn(err.message);
-          return;
-        }
-
-        files.forEach((file) => {
-          const pathFile = path.join(pathFolder, file);
-
-          fs.stat(pathFile, (err, stats) => {
-            if (err) {
-              this.logger.warn(err.message);
-              return;
-            }
-
-            const diff = Math.abs(new Date().getTime() - stats.mtime.getTime());
-
-            if (stats.isFile() && diff > 3600000) {
-              fs.unlink(pathFile, (err) => {
-                if (err) {
-                  this.logger.warn(err.message);
-                  return;
-                }
-              });
-            }
-          });
-        });
-      });
     });
   }
 }
