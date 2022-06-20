4b7db32d9a917bc294c969e96d0687ebda17249f
adikalon <webadev@yandex.ru>
2022-06-20 15:55:10 +0300

    Защита от подбора пароля

--- server/src/common/guards/admin.guard.ts
+++ server/src/common/guards/admin.guard.ts
@@ -1,6 +1,7 @@
 import {
   CanActivate,
   ExecutionContext,
+  ForbiddenException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -9,17 +10,26 @@ import { EnvironmentVariables } from '../../env.validation';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
+  private static date = new Date();
+
   constructor(
     private readonly configService: ConfigService<EnvironmentVariables, true>,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const date = new Date();
+
+    if (AdminGuard.date > date) {
+      throw new ForbiddenException(`Forbidden to ${AdminGuard.date}`);
+    }
+
     const req = context.switchToHttp().getRequest();
     const res = context.switchToHttp().getResponse();
     const authToken = req.header('authorization');
     const adminToken = `Bearer ${this.configService.get('ADMIN_TOKEN')}`;
 
     if (!authToken || authToken !== adminToken) {
+      AdminGuard.date = new Date(new Date().getTime() + 5 * 60000);
       res.set({ 'WWW-Authenticate': 'Bearer realm="Space (ADMIN_TOKEN)' });
       throw new UnauthorizedException();
     }
