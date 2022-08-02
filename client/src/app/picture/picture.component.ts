46c9612ed6370a9b3d801f27748977555a7ace47
adikalon <webadev@yandex.ru>
2022-08-02 21:39:26 +0300

    Фильтр

--- client/src/app/picture/picture.component.ts
+++ client/src/app/picture/picture.component.ts
@@ -7,11 +7,18 @@ import { PictureService } from './picture.service';
 @Component({
   selector: 'app-picture',
   templateUrl: './picture.component.html',
+  styleUrls: ['./picture.component.scss'],
   providers: [MessageService, PictureService],
 })
 export class PictureComponent implements OnInit {
   tags: TagInterface[] = [];
   showTags = false;
+  showFilter = false;
+  searchText = '';
+  minWidth: number | null = null;
+  maxWidth: number | null = null;
+  minHeight: number | null = null;
+  maxHeight: number | null = null;
 
   constructor(
     private readonly pictureService: PictureService,
@@ -37,6 +44,52 @@ export class PictureComponent implements OnInit {
   }
 
   async search(params: any) {
+    await this.router.navigate(['/'], {
+      queryParams: params,
+    });
+  }
+
+  async switchTags() {
+    this.showTags = !this.showTags;
+
+    if (this.showFilter) {
+      this.showFilter = false;
+    }
+  }
+
+  async switchFilter() {
+    this.showFilter = !this.showFilter;
+
+    if (this.showTags) {
+      this.showTags = false;
+    }
+  }
+
+  async getResult() {
+    const params: any = {};
+
+    if (this.searchText) {
+      params['search'] = this.searchText;
+    }
+
+    if (this.showFilter) {
+      if (this.minWidth) {
+        params['fromWidth'] = this.minWidth;
+      }
+
+      if (this.minHeight) {
+        params['fromHeight'] = this.minHeight;
+      }
+
+      if (this.maxWidth) {
+        params['toWidth'] = this.maxWidth;
+      }
+
+      if (this.maxHeight) {
+        params['toHeight'] = this.maxHeight;
+      }
+    }
+
     await this.router.navigate(['/'], {
       queryParams: params,
     });
