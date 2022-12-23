e49d85585384e182e77ea86f4f85db939a9b9ef6
adikalon <webadev@yandex.ru>
2022-12-23 16:33:09 +0300

    Начало админки и вход в нее

---
+++ client/pages/admin/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card mb-3 login-block">
+    <div class="card-body">
+      <h5 class="card-title">
+        Enter access key
+      </h5>
+      <div class="form-group has-danger key-field">
+        <input type="text" placeholder="Access key" class="form-control is-invalid">
+        <div class="invalid-feedback">
+          Sorry, that username's taken. Try another?
+        </div>
+      </div>
+      <div class="d-grid gap-2 key-button">
+        <button class="btn btn-lg btn-primary" type="button">
+          Login
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .login-block {
+    max-width: 450px;
+    margin: 0 auto;
+    margin-top: 30px;
+    margin-bottom: 0 !important;
+  }
+
+  .key-field {
+    margin-top: 15px;
+  }
+
+  .key-button {
+    margin-top: 15px;
+  }
+</style>
