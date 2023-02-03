26a110f1b78b2cd97ff9ab978729cb487046f023
adikalon <webadev@yandex.ru>
2023-02-03 16:52:53 +0300

    Страница ошибки

---
+++ client/layouts/error.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="error-block">
+    <h2 class="status">
+      {{ error.statusCode }}
+    </h2>
+    <h1 class="message">
+      {{ error.message }}
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default () {
+        return {}
+      }
+    }
+  },
+  head () {
+    return {
+      title: `${this.error.message} - ${process.env.appName}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .error-block {
+    margin-top: 30px;
+    text-align: center;
+  }
+
+  .status {
+    font-size: 130px;
+  }
+
+  .message {
+    font-size: 30px;
+  }
+</style>
